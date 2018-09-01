d0fda7db96d1e5c6d82880944cfa41836e530273
FIIRETURRET <brandonjoutras@gmail.com>
2018-09-01 09:01:32 -0500

    Create UnkownPastry.java

---
+++ Assignmnet 1 Decorating/src/UnkownPastry.java
@@ -0,0 +1,22 @@
+
+public class UnknownPastry extends Pastry{
+	public UnknownPastry() {
+		description = "An unknown pastry";
+	}
+	
+	public double cost() {
+		return 0;
+	}
+
+	@Override
+	public String getEncounter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void solveEncounter(String s) {
+		// TODO Auto-generated method stub
+		
+	}
+}
